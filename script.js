54e42b7c04117b754f1f40a33b87e256c056db32
Ranita Ghosh <ranitaghosh04@gmail.com>
2024-02-28 21:55:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let mainDivEle = document.getElementById('mainDiv');
+
+mainDivEle.style.backgroundColor = "yellow";
+mainDivEle.style.margin = "20px";
+mainDivEle.style.padding = "10px";
+mainDivEle.style.fontSize = "18px";
+mainDivEle.style.fontWeight = "bold";
+mainDivEle.style.height = "200px";
+mainDivEle.style.width = "300px";
+mainDivEle.style.color = "blue";
